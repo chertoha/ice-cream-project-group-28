34e55e517e1a9c7435dd4f12f5a83f3282d5f9a2
the-most-trained-trainee <oleg.lavrenko@outlook.com>
2022-07-17 00:35:28 +0300

    Lavr Interim Commit 16072022
    4 модалки в предварительном виде. Еще 1) исправлять косяки, 2) делать общие классы, 3) править скрипты, чтобы работало для всех каунтеров для 1 модалки

--- src/mob-menu__modal.js
+++ src/mob-menu__modal.js
@@ -22,4 +22,29 @@
         toggleMenu();
       }
     }
-  })();
+})();
+  
+
+
+const inc = document.getElementsByClassName("stepper");
+for (i = 0; i < inc.length; i++) {
+  let incI = inc[i].querySelector("input"),
+    id = incI.getAttribute("id"),
+    min = incI.getAttribute("min"),
+    max = incI.getAttribute("max"),
+    step = incI.getAttribute("step");
+  document.getElementById(id).previousElementSibling.setAttribute("onclick", "stepperInput('" + id + "', -" + step + ", " + min + ")");
+  document.getElementById(id).nextElementSibling.setAttribute("onclick", "stepperInput('" + id + "', " + step + ", " + max + ")");
+}
+function stepperInput(id, s, m) {
+  const el = document.getElementById(id);
+  if (s > 0) {
+    if (parseInt(el.value) < m) {
+      el.value = parseInt(el.value) + s;
+    }
+  } else {
+    if (parseInt(el.value) > m) {
+      el.value = parseInt(el.value) + s;
+    }
+  }
+}
